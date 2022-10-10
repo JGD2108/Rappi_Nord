d8fcc640a7960bcc5a3ad26842fe0d1997053f08
JGD2108 <90580764+JGD2108@users.noreply.github.com>
2022-10-10 11:58:09 -0500

    Update user.py

--- user.py
+++ user.py
@@ -1,4 +1,5 @@
-class User:
+from restaurantes import Cafe, Terrase
+class User(Cafe):
     def __init__(self, nombre: str, id: int, ubicacion: str, tel: int) -> None:
         self.nombre = nombre
         self.id = id
@@ -8,8 +9,22 @@ class User:
     def pago():
             pass
 
-    def pedido():
-            pass
+    def pedido(self):
+            print("Escoja el restaurante a escoger")
+            answer= input("1. Cafe, 2. Terrase")
+            while(answer!="1" and answer!="2"):
+                print("escoja 1 o 2")
+                answer= input("1. Cafe '\t' 2. Terrase")
+            if answer=="1":
+                total=0
+                while True:
+                    item=input("Item:").title()
+                    if item in Cafe.menuC:
+                        total+=Cafe.menuC[item]
+                        print("Total:$",end="")
+                        print("{:.2f}".format(total))
+                    elif(item=="Stop"):
+                        break
     
     def __repr__(self) -> str:
         pass

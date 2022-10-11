7e6663f897d560a23230d41c2ebcdc38bad44281
JGD2108 <gomezdelahoz@uninorte.edu.co>
2022-10-11 17:56:11 -0500

    User.py

--- user.py
+++ user.py
@@ -1,30 +1,52 @@
 from restaurantes import Cafe, Terrase
-class User(Cafe):
-    def __init__(self, nombre: str, id: int, ubicacion: str, tel: int) -> None:
+class User(Cafe,Terrase):
+    def __init__(self, nombre: str, id: int, ubicacion: str, tel: int, carrito:list,Total:int) -> None:
         self.nombre = nombre
         self.id = id
         self.ubicacion = ubicacion
         self.tel = tel
         
-    def pago():
-            pass
+    def pedido(self,Total):
+        carrito=[]
+        print("Escoja el restaurante a escoger")
+        answer= input("1. Cafe, 2. Terrase")
+        while(answer!="1" and answer!="2"):
+            print("escoja 1 o 2")
+            answer= input("1. Cafe '\t' 2. Terrase")
+        if answer=="1":
+            print("Digite stop cuando desee parar")
+            while True:
+                item=input("Item:").title()
+                if item in Cafe.menuC:
+                    Total+=Cafe.menuC[item]
+                    carrito.append(item)
+                elif(item=="Stop"):
+                    break
+            print(f"Su total es: {Total}")
+        elif(answer=="2"):
+            print("Digite stop cuando desee parar")
+            while True:
+                item=input("Item:").title()
+                if item in Terrase.menuT:
+                    Total+=Terrase.menuT[item]
+                    carrito.append(item)
+                elif(item=="Stop"):
+                    break
+            print(f"Su total es: {Total}")
+            for a in carrito:
+                print (a)
+        return Total,carrito
 
-    def pedido(self):
-            print("Escoja el restaurante a escoger")
-            answer= input("1. Cafe, 2. Terrase")
-            while(answer!="1" and answer!="2"):
-                print("escoja 1 o 2")
-                answer= input("1. Cafe '\t' 2. Terrase")
-            if answer=="1":
-                total=0
-                while True:
-                    item=input("Item:").title()
-                    if item in Cafe.menuC:
-                        total+=Cafe.menuC[item]
-                        print("Total:$",end="")
-                        print("{:.2f}".format(total))
-                    elif(item=="Stop"):
-                        break
+    def pago(self):
+            print("Digite con que desea pagar")
+            while True:
+                print("1. Datafono")
+                print("2. Efectivo")
+                opc=input()
+                if opc=="1" or opc=="2":
+                    break
+            if opc=="1":
+                pass
     
     def __repr__(self) -> str:
         pass

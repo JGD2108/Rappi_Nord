b990526f1ca4286f9deed9a6de37f85772800b61
JGD2108 <gomezdelahoz@uninorte.edu.co>
2022-11-01 09:22:23 -0500

    User.py

--- Domiciliarios.py
+++ Domiciliarios.py
@@ -1,33 +1,18 @@
 import sqlite3
 con = sqlite3.connect("Domiciliario.db")
 cur = con.cursor()
-cur.execute("DROP TABLE IF EXISTS RegDomi")
-sql="""
-    CREATE TABLE RegDomi(
-        Name TEXT,
-        ID INT,
-        Cel INT
-    )
-"""
+###cur.execute("DROP TABLE IF EXISTS RegDomi")
 
-"""
-Para meter datos en la base de datos: 
-quitas el execute de Drop table if exists y  todo de ahí para abajo 
 data = [
-        ("Baja Taco", 4.00),
-        ("Burrito", 7.50),
-        ("Bowl", 8.50),
-        ("Nachos", 11.00),
-        ("Quesadilla", 8.50),
-        ("Super Burrito", 8.50),
-        ("Super Quesadilla", 9.50),
-        ("Taco", 3.00),
-        ("Tortilla Salad", 8.00) ]
+        ("Jose David",1001915145, 3043774896),
+        ("Cristian", 1001915145, 310554675),
+         ]
 
-cur.executemany("INSERT INTO MENU VALUES(?, ?)", data)
+cur.executemany("INSERT INTO RegDomi VALUES(?, ?, ?)", data)
 con.commit()
 """
 
 cur.execute(sql)
 con.commit()
 con.close()
+"""
--- domiciliario.py
+++ domiciliario.py
@@ -1,10 +1,75 @@
+from abc import abstractmethod,ABCMeta
+import json
+from typing import Dict
+class InternalState(metaclass= ABCMeta):
+    @abstractmethod
+    def changeState(self):
+        pass
+class Available(InternalState):
+    def changeSate(self):
+        print("Available Person")
+
+class Occupied(InternalState):
+    def changeSate(self):
+        print("Occupied Person")
+
 class Domiciliario:
     def __init__(self, nombreD: str, telD: int) -> None:
         self.nombreD = nombreD
         self.telD = telD
+        self.state = None
 
-    def obtener_rating():
-        pass
+    def setState(self,status):
+        self.state=status
 
-    def disponibilidadD():
-        pass
+    def getState(self):
+        return self.state
+    
+    def changeState(self):
+        self.state.changeState()
+
+
+class Flyweight():
+    """
+    The Flyweight stores a common portion of the state (also called intrinsic
+    state) that belongs to multiple real business entities. The Flyweight
+    accepts the rest of the state (extrinsic state, unique for each entity) via
+    its method parameters.
+    """
+    def __init__(self, sharedState:str) -> None:
+        self.sharedState = sharedState
+    def operation(self, uniqueState:str) -> None:
+        s = json.dumps(self._sharedState)
+        u = json.dumps(uniqueState)
+        
+
+class FlyweightFactory():
+    """
+    The Flyweight Factory creates and manages the Flyweight objects. It ensures
+    that flyweights are shared correctly. When the client requests a flyweight,
+    the factory either returns an existing instance or creates a new one, if it
+    doesn't exist yet.
+    """
+    _domiciliarios: Dict[str, Flyweight]={}
+
+    def __init__(self, initialFlyweight: Dict) -> None:
+        for state in initialFlyweight:
+            self._domiciliarios[self.get_key(state)] = Flyweight(state)
+    
+    def get_key(self, state:Dict) -> None:
+        return "_".join(sorted(state))
+
+    def getFlyweight(self,shared_state:Dict) -> Flyweight:
+        """
+        Returns an existing Flyweight with a given state or creates a new one.
+        """
+        key = self.get_key(shared_state)
+
+        if not self._domiciliarios(key):
+            self._domiciliarios[key]= Flyweight(shared_state)
+        
+        return self._domiciliarios[key]
+    
+    def listFlyweights(self)->None:
+        count = len(self._domiciliarios)
+

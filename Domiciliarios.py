de3600e5c2d54160a777a7b26a7a2f504cf75e5c
JGD2108 <gomezdelahoz@uninorte.edu.co>
2022-11-05 19:37:34 -0500

    User.py

--- Domiciliarios.py
+++ Domiciliarios.py
@@ -1,18 +1,15 @@
 import sqlite3
 con = sqlite3.connect("Domiciliario.db")
 cur = con.cursor()
-###cur.execute("DROP TABLE IF EXISTS RegDomi")
+cur.execute("DROP TABLE IF EXISTS RegDomi")
+
 
 data = [
-        ("Jose David",1001915145, 3043774896),
-        ("Cristian", 1001915145, 310554675),
+        ("Jose David",1001915145, 3043774896, "Available"),
+        ("Cristian", 1001915145, 310554675, "Available"),
          ]
 
-cur.executemany("INSERT INTO RegDomi VALUES(?, ?, ?)", data)
-con.commit()
-"""
+cur.executemany("INSERT INTO Domiciliarios VALUES(?, ?, ?,?)", data)
 
-cur.execute(sql)
 con.commit()
 con.close()
-"""

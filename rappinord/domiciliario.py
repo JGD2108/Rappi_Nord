cd1ca1ce9b4c42c5946ebbeb024b8d6999d172ac
cfvizcaino <cfvizcaino@uninorte.edu.co>
2022-11-15 19:31:50 -0500

    asd

--- rappinord/domiciliario.py
+++ rappinord/domiciliario.py
@@ -1,6 +1,3 @@
-from __future__ import annotations
-from abc import ABC, abstractmethod
-import sqlite3
 
 class Domiciliario:
     def __init__(self, name: str, id: int, cel: int, state: str, Domi: list) -> None:
@@ -23,21 +20,6 @@ class Domiciliario:
         x="Occupied"
         self.Domi[key] = x
         return self.Domi
-
-    con = sqlite3.connect("Domiciliario.db")
-    cur = con.cursor()
-    x=[]
-    for row in cur.execute("SELECT NAME FROM Domiciliarios"): ##Seleccionar los nombres del sql
-        str = ''
-        for item in row:
-            str = str + item
-        x.append(str)
-    Domi={}
-
-    for i in range (len(x)):
-            Name = x[i]
-            new = {Name:"Occupied"}
-            Domi.update(new)
 
 class process():
     def execute():
